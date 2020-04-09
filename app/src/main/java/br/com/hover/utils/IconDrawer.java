ba130d7a058c7d33025e3cd290e2ed74d3eb076b
Lionan Dantas <lionan.ferreira@globalsys.com.br>
2020-04-09 09:24:02 -0300

    add lib utils

---
+++ app/src/main/java/br/com/hover/utils/IconDrawer.java
@@ -0,0 +1,37 @@
+package br.com.hover.utils;
+
+import android.graphics.Bitmap;
+
+public class IconDrawer {
+    private Bitmap icon;
+    private float margin;
+    private int backgroundColor;
+
+    //Setters
+    public IconDrawer (Bitmap icon, int backgroundColor) {
+        this.icon = icon;
+        this.backgroundColor = backgroundColor;
+    }
+
+    //Getters
+    public int getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public Bitmap getIcon() {
+        return icon;
+    }
+
+    public float getMargin() {
+        return margin;
+    }
+
+    /**
+     * @param margin Margin to be left outside the icon
+     * @return IconDrawer for chaining
+     */
+    public IconDrawer setMargin(float margin) {
+        this.margin = margin;
+        return this;
+    }
+}
